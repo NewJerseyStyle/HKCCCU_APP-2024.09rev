a11fba3323af29a8a6f67c158d6b51c1a5ed03df
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2024-07-02 17:31:11 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,54 @@
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate serde;
 use rocket::fs::NamedFile;
 use rocket::get;
+use rocket::request::Form;
+use chrono::NaiveDate;
+use validator::{validate, Validate};
+use regex::Regex;
+
+#[derive(Serialize, Deserialize)]
+struct UserLogin {
+    name: String,
+    password: String,
+}
+
+#[derive(Serialize, Deserialize, Validate)]
+pub struct UserRegistration {
+    name: String,
+    #[validate(custom = "validate_password")]
+    password: String,
+    #[validate(email)]
+    email: String,
+    date_of_birth: NaiveDate,
+    gender_description: Option<String>,
+}
+
+fn validate_password(input: &String) -> Result<(), validator::ValidationFieldError> {
+    let re = Regex::new(r"^[0-9a-fA-F]{40}$").unwrap();
+    if re.is_match(input) {
+        Ok(())
+    } else {
+        Err(validator::ValidationFieldError::with_message("密码不符合规则", "password"))
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct MovieRecord {
+    title: String,
+    director: String,
+    starring: Vec<String>,
+    details: Option<String>,
+    staffs: Option<Vec<String>>,
+    rental_price: Option<f64>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct MovieRentalRecord {
+    user_id: i32,
+    movie_id: i32,
+    rental_date: NaiveDate
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -17,14 +65,24 @@ fn browse(id: &int) -> &'static str {
     "Hello, world!"
 }
 
-#[post("/add-item", data = "<item>")]
-fn add_item(login: Form<MovieRecord>) -> String {
+#[post("/wish-item/<movie_id>")]
+fn wish_item(movie_id: &int) -> String {
     format!("Hello, {}!", login.name)
 }
 
-#[post("/register", data = "<login>")]
-fn login(login: Form<UserLogin>) -> String {
-    format!("Hello, {}!", login.name)
+#[post("/rent-item", data = "<item>")]
+fn rent_item(item: Form<MovieRentalRecord>) -> String {
+    format!("Hello, {}!", item.name)
+}
+
+#[post("/add-item", data = "<item>")]
+fn add_item(item: Form<MovieRecord>) -> String {
+    format!("Hello, {}!", item.name)
+}
+
+#[post("/register", data = "<data>")]
+fn register(data: Form<UserRegistration>) -> String {
+    format!("Hello, {}!", data.name)
 }
 
 #[post("/login", data = "<login>")]
@@ -50,6 +108,7 @@ async fn serve_search() -> Result<NamedFile, std::io::Error> {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/api", routes![index,])
+    rocket::build().mount("/api", routes![index, search, browse,
+                                          add_item, rent_item, wish_item])
     rocket::build().mount("/", routes![serve_index, serve_login, serve_search])
 }
