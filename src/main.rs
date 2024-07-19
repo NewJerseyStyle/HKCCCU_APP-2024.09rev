2378893801a9338b2ccdd7114eee934c7b92bad4
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2024-07-19 13:56:36 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -213,9 +213,9 @@ async fn register(data: Json<UserRegistration>, pool: &State<DbPool>) -> Result<
     })))?;
 
     let new_user = models::User {
-        username: &data.username,
-        password_hash: &hashed_password,
-        email: &data.email,
+        Username: &data.username,
+        PasswordHash: &hashed_password,
+        Email: &data.email,
         date_of_birth: data.date_of_birth,
         gender_description: data.gender_description.as_deref(),
     };
